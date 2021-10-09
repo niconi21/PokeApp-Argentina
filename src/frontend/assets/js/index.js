7de4946d63be01c45e4cce88a128a9e31d1ffe46
Ivan Cordova <talivan1602@gmail.com>
2021-10-09 04:10:00 -0500

    Pruebas consumo Api

--- src/frontend/assets/js/index.js
+++ src/frontend/assets/js/index.js
@@ -5,3 +5,35 @@ $('#btn-menu').click(() => {
 
     }
 })
+
+function createFieldSearch(datos) {
+    return $.ajax({
+        url: `${datos}`,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (response) {
+        return response;
+    });
+}
+
+$('#prueba').click(()=>{
+    var idPokemon;
+    createFieldSearch("https://pokeapi.co/api/v2/pokemon/").done((resultado)=>{
+        //Nombre.................................................
+        let pokemon = resultado.results;
+        console.log("Nombre: "+pokemon[0].name)
+        //Sprits.................................................
+        createFieldSearch(`${pokemon[0].url}`).done((resultado)=>{
+            let pokemonSprits = resultado.sprites;
+            console.log("Sprit: "+pokemonSprits.front_default)
+            $("#imagen1").attr("src",`${pokemonSprits.front_default}`)
+            console.log("id: "+resultado.id)
+        });
+        //descriptions.............................................
+        console.log("var: "+idPokemon)
+/*         createFieldSearch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}/`).done((resultado)=>{
+            let pokemonDescriptions = resultado.descriptions;
+            console.log("Descripcion: "+pokemonDescriptions[1].description)
+        }); */
+    });
+});
