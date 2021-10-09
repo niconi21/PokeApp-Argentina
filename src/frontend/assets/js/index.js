8b099c0107fd477f4020803c22d998dcddb97145
Ivan Cordova <talivan1602@gmail.com>
2021-10-09 16:46:20 -0500

    Funcionalidad del boton Ver mas

--- src/frontend/assets/js/index.js
+++ src/frontend/assets/js/index.js
@@ -16,18 +16,22 @@ function createFieldSearch(datos) {
     });
 }
 
+$("#botonMas").click(()=>{
+    llenarCards(localStorage.getItem("nextUrl"))
+})
 
-
-function llenarCards(){
-    createFieldSearch("https://pokeapi.co/api/v2/pokemon/").done((resultado)=>{
+function llenarCards(url){
+    createFieldSearch(`${url}`).done((resultado)=>{
         //Nombre.................................................
         let consulta = resultado.results;
+        let urlNext = resultado.next;
+        localStorage.setItem("nextUrl", urlNext);
         consulta.forEach(pokemon => {
             var nombrePokemon = pokemon.name;
             createFieldSearch(`${pokemon.url}`).done((resultado)=>{
                 let pokemonSprits = resultado.sprites;
-                createFieldSearch(`https://pokeapi.co/api/v2/characteristic/${resultado.id}/`).done((resultado)=>{
-                    let pokemonChar = resultado.descriptions;
+                createFieldSearch(`https://pokeapi.co/api/v2/ability/${resultado.id}/`).done((resultado)=>{
+                    let pokemonChar = resultado.effect_entries;
                     $("#cardsPokemon").append(`
                     <div class="card-producto border-secondary elevation-2 border-left-danger border-right-danger" id="card1">
                         <div class="card-producto-header">
@@ -35,20 +39,22 @@ function llenarCards(){
                         </div>
                         <div class="card-producto-body">
                             <small class="text-warning">${nombrePokemon}</small>
-                            <p>${pokemonChar[1].description}</p>
+                            <p>${pokemonChar[1].short_effect}</p>
                         </div>
                         <div class="card-producto-footer">
                             <button class="btn btn-info text-light" id="prueba"><i class="text-dark fas fa-share"></i>
                                 Compartir</button>
                         </div>
                     </div>`);
-    
+
                 });
             }); 
         });
     });
 }
 
+
 $(document).ready(()=>{
-        llenarCards();
+    llenarCards("https://pokeapi.co/api/v2/pokemon/");
 })
+
