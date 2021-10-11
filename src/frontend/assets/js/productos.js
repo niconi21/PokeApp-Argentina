81e59508fe807a94aa9761eac484fb49c9c6adf8
Ivan Cordova <talivan1602@gmail.com>
2021-10-11 15:11:48 -0500

    Funcion Buscar terminada

--- src/frontend/assets/js/productos.js
+++ src/frontend/assets/js/productos.js
@@ -9,14 +9,36 @@ function createFieldSearch(datos) {
     });
 }
 
-/* $("#botonMas").click(()=>{
-    llenarCards(localStorage.getItem("nextUrl"))
-}) */
+function llenarCardsItems(url){
+    $("#cardsPokemon").empty();
+    createFieldSearch(`${url}`).done((resultado)=>{
+        let nombreItem = resultado.name;
+        let costItem = resultado.cost;
+        let effectItem = resultado.effect_entries[0].effect;
+        let spriteItem = resultado.sprites.default;
+        $("#cardsPokemon").append(`
+        <div class="card-producto border-secondary elevation-2" id="card1">
+            <div class="card-producto-header">
+                <img src="${spriteItem}" width="100px">
+            </div>
+            <div class="card-producto-body">
+                <h2 class="text-warning">${nombreItem}: $${costItem}</h2>
+                <p class="text-justify">${effectItem}</p>
+            </div>
+            <div class="card-producto-footer">
+                <button class="btn btn-info text-light" id="prueba"><i class="text-dark fas fa-share"></i>
+                    Compartir</button>
+            </div>
+        </div>`);
+    })
+}
+
 
 function llenarCardsPokemon(url){
     $("#cardsPokemon").empty();
     createFieldSearch(`${url}`).done((resultado)=>{
         let pokemonSprits = resultado.sprites;
+        let namePokemon = resultado.name;
         createFieldSearch(`https://pokeapi.co/api/v2/ability/${resultado.id}/`).done((resultado)=>{
             let pokemonChar = resultado.effect_entries;
             $("#cardsPokemon").append(`
@@ -25,7 +47,7 @@ function llenarCardsPokemon(url){
                     <img src="${pokemonSprits.front_default}" width="100px">
                 </div>
                 <div class="card-producto-body">
-                    <h2 class="text-warning">${resultado.name}</h2>
+                    <h2 class="text-warning">${namePokemon}</h2>
                     <p class="text-justify">${pokemonChar[1].short_effect}</p>
                 </div>
                 <div class="card-producto-footer">
@@ -70,18 +92,16 @@ function llenarCardsBerry(url){
 $(document).ready(()=>{
     $("#boton1").click(function() {
         let elemntBusqueda = $("#busqueda").val();
-        console.log(elemntBusqueda)
         let radioValue = $('input[name="radioB"]:checked').val();
-        console.log (radioValue)
 
         if(radioValue == "pokemon"){
             llenarCardsPokemon(`https://pokeapi.co/api/v2/pokemon/${elemntBusqueda}`);
         }
         if(radioValue == "berries"){
-            llenarCardsBerry(`https://pokeapi.co/api/v2/berry/${elemntBusqueda}`)
+            llenarCardsBerry(`https://pokeapi.co/api/v2/berry/${elemntBusqueda}`);
         }
         if(radioValue == "items"){
-            console.log ("Buscar items")
+            llenarCardsItems(`https://pokeapi.co/api/v2/item/${elemntBusqueda}`);
         }
     });
 })
