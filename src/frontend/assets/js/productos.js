78b532aa3dc1160e8e6ac24fb7941a94ccdd0202
Ivan Cordova <talivan1602@gmail.com>
2021-10-11 18:01:38 -0500

    Cambiso en productos

--- src/frontend/assets/js/productos.js
+++ src/frontend/assets/js/productos.js
@@ -1,4 +1,12 @@
 
+$('#btn-menu').click(() => {
+    let items = document.getElementsByClassName('nav-item');
+    for (let index = 0; index < items.length; index++) {
+        items[index].classList.toggle('oculto')
+
+    }
+})
+
 function createFieldSearch(datos) {
     return $.ajax({
         url: `${datos}`,
