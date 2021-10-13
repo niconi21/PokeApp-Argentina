ed4f0d09a8bb5868c24903d714a963aa83db0bd4
Nicolas Moreno Duran <morenodurann@gmail.com>
2021-10-13 15:19:10 -0500

    carrito responsivo

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -9,11 +9,11 @@ function llenarTabla(){
     pokemonesCarrito.forEach((pokemon, index) => {
         filasPokemones += `
         <tr>
-            <td>${pokemon.nombre}</td>
-            <td><img src="${pokemon.img}"></td>
+            <td class="text-center">${pokemon.nombre}</td>
+            <td class="text-center"><img src="${pokemon.img}"></td>
             <td>${pokemon.efecto}</td>
-            <td>$${pokemon.costo}.00</td>
-            <td><button onclick="eliminarCarrito(${index})" class="btn btn-danger"><i class="fas fa-trash"></i></button></td>
+            <td class="text-center">$${pokemon.costo}.00</td>
+            <td class="text-center"><button onclick="eliminarCarrito(${index})" class="btn btn-danger"><i class="fas fa-trash"></i></button></td>
         </tr>
         `  
         costo+=parseInt(pokemon.costo);
